207b1d230e12ddb64129c84004b4202cfd239b81
M.Bharath Bhushan <mbharath278@hotmail.com>
2018-12-22 11:01:07 +0530

    03,04 -MVCDemo, Controller, Routing
    03,04 -MVCDemo, Controller, Routing

---
+++ 03 - Creating First Application/03 - Creating First Application/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03___Creating_First_Application.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public string Index(int? id)
+        {
+            return "Id :" + (id.HasValue ? id.ToString() : "null");
+        }
+    }
+}
---
+++ 03 - Creating First Application/03 - Creating First Application/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace _03___Creating_First_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["MVCVersion"] = typeof(Controller).Assembly.GetName().Version.ToString();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
